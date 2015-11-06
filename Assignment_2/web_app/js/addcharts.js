22ebec5c658ea13c6a4265ac997c6b2346cb0235
Katie Lin <kl2532@columbia.edu>
2015-11-06 03:53:46 -0500

    Added infinite AJAX

--- Assignment_2/web_app/js/addcharts.js
+++ Assignment_2/web_app/js/addcharts.js
@@ -1,5 +1,25 @@
-var ctx = document.getElementById("foodPieChart").getContext("2d");
-var data = [
+var updateChart = function(data){
+	console.log("update chart");
+	console.log(data);
+	$('.foodPieChart').remove(); // this is my <canvas> element
+  	$('#pie-chart-holder').append("<canvas id= 'foodPieChart' width='300' height='300' style='text-align:center;'></canvas>");
+	var ctx = document.getElementById("foodPieChart").getContext("2d");
+	var myPieChart = new Chart(ctx).Pie(data);
+}
+var addChart = function(data){
+	console.log("new chart");
+	var ctx = document.getElementById("foodPieChart").getContext("2d");
+	var myPieChart = new Chart(ctx).Pie(data);
+}
+
+var updateSubheading = function(){
+	document.getElementById("data-placeholder").innerHTML = "<br><br><h2>Here's what she ate:</h2>\
+                    <div class='pie-chart-holder'>\
+                        <canvas id= 'foodPieChart' width='300' height='300' style='text-align:center;'></canvas>\
+                    </div>";
+}
+
+var test_data = [
     {
         value: 300,
         color:"#F7464A",
@@ -19,4 +39,27 @@ var data = [
         label: "Yellow"
     }
 ];
-var myPieChart = new Chart(ctx).Pie(data);
+
+var dataURL = "/get_data";
+
+var InfiniteAjaxRequest = function (uri) {
+    $.ajax({
+        url: uri,
+        success: function(data) {
+            // do something with "data"
+            updateChart(data);
+            InfiniteAjaxRequest (uri);
+        },
+        error: function(xhr, ajaxOptions, thrownError) {
+            alert(thrownError);
+        }
+    });
+};
+
+document.getElementById('data-form').addEventListener('submit', function (e) {
+	e.preventDefault();
+    updateSubheading();
+    //addChart(test_data);
+    updateChart(test_data);
+    InfiniteAjaxRequest (dataURL);
+}, false);
