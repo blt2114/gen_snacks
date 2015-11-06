f1a7bb7ecb324b3da8035a98431a5029c7a4a1bf
eodnhoj <michael@modernliteracy.org>
2015-11-05 21:43:24 -0500

    Huge changes to blastquery.

--- Assignment_2/blastquery.py
+++ Assignment_2/blastquery.py
@@ -3,7 +3,8 @@ import argparse
 import threading
 import time
 import random
-import Queue
+
+from Queue import Queue
 from pprint import pprint
 
 import poretools
@@ -14,6 +15,7 @@ from Bio.Blast import NCBIWWW
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-s", "--seq", nargs=1, help="The nucleotide sequence string to search.")
+parser.add_argument("-f", "--file", nargs=1, help="For single FAST5 files.")
 parser.add_argument("-fd", "--filedir", nargs=1, help="Path to FAST files to parse through.")
 
 # TODO: CHANGE THIS AS NECESSARY
@@ -50,25 +52,67 @@ def multiquery(seqs, paths):
     Limits:
     Queries spaced at least 3 seconds apart
     """
-   # threads = threading.activeCount()
-   # lastRun = time.time()
-   # while(threads < 3 and time.time() - lastrun > 3):
-   #     threading.thread(target:query(seqs[0]))
-   #     lastrun = time.time()
-   #     seqs = seqs[1:]
+    queue = Queue()
+    max_threads = 5
+    threadlist = range(max_threads)
+    timecheck = time.time()
 
-    # We're swapping out the extension from fast5 to txt for permanent
-    # search result storage
+    # Creating the writepaths
     paths = [p[:-5] + "xml" for p in paths]
     paths = [DATADUMPPATH + p.split("/")[-1] for p in paths]
     totalset = zip(seqs, paths)
+
+    def threadFunc(q=queue):
+        while(True):
+            seq,path = q.get()
+            print("Writing to %s." % path)
+            try:
+                results = query(seq,path)
+                print("%s done." % path)
+            except Exception as e:
+                print("ERROR: %s\n%s" % (path,e))
+                time.sleep(random.randint(3,5))
+                continue
+            q.task_done()
+
+    # Loading up the Queue
+    print("Loading up Queue.")
+    for e in totalset:
+        queue.put(e)
     
+    print("Executing threads.")
+    for t in range(max_threads):
+        time.sleep(3)
+        threadlist[t] = threading.Thread(target=threadFunc, args=(queue,))
+        threadlist[t].daemon = True
+        threadlist[t].start()
+
+    print("Attempting to join threads.")
+    queue.join()
+    """ 
     for seq,path in totalset:
         print("Writing to %s." % path)
         try:
             q = query(seq,path)
         except Exception as e:
             print("ERROR: %s\n%s" % (path,e))
+    """
+def getWritePath(filepath, writepath=DATADUMPPATH):
+    """
+    Take in a .fast5 path and convert it to the writepath.
+    """
+    assert(filepath[-5:] == 'fast5')
+    filepath = filepath[:-5]
+    filepath = filepath.split("/")[-1]
+    return DATADUMPPATH + filepath
+
+def getSeq(filepath):
+    """
+    Take in a .fast5 path and get the 2D sequence back.
+    """
+    t = poretools.Fast5FileSet([filepath])
+    for e in t:
+        return e.get_fasts("2D")[0].seq
 
 def prune(fileset, filepath=DATADUMPPATH):
     """
@@ -108,6 +152,7 @@ def multiseq(filepath):
     fileset = fileset[:4]
     fastset = []
     
+    print("Getting sequences from FAST5 files.")
     # We're doing this because we don't know if Fast5FileSet does things in order
     for fp in fileset:
         tmp = [fp]
@@ -118,13 +163,20 @@ def multiseq(filepath):
 
     # DEBUG: This assert makes sure things line up
     assert(len(fastset) == len(fileset))
-
+    
+    print("Beginning query process.")
     # Passing it off to the query function
     multiquery(fastset, fileset)
 
 if __name__ == "__main__":
     args = parser.parse_args()
-    if(args.filedir):
+    if(args.file):
+        # TODO: UNTESTED.
+        arg = args.file[0]
+        seq = getSeq(arg)
+        writepath = getWritePath(args.file[0])
+        query(seq,writepath)
+    elif(args.filedir):
         multiseq(args.filedir[0])
     elif(args.seq):
         query(args.seq[0])
